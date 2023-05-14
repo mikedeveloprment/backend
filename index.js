712e8e406269f71e25f64b1b1c93a978bdc41895
mikedeveloprment <mikebahgneiy@gmail.com>
2023-05-15 00:08:00 +0300

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ import cors from "cors";
 // тут ми просто робим підключеня
 // до бази даних як в fetch но пмшем mongoose.connect
 mongoose
-	.connect(process.env.MONGODB_URL)
+	.connect("mongodb+srv://admin:WWW@cluster0.mtojhza.mongodb.net/blog")
 	.then(() => console.log("база даних підключена"))
 	.catch((err) => console.log("помилка", err));
 
@@ -95,7 +95,7 @@ app.patch(
 
 // app.listen встановлює порт і робить вказану функцію при запусці
 /* ошибка якшо є така */
-app.listen(process.env.PORT ||  1000, (err) => {
+app.listen(1000, (err) => {
 	if (err) {
 		return console.log(err);
 	}
